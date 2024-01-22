c9a95069a45b79b767b2da107943d762de2ccaf1
Lucas Priestley <lmpriestley@outlook.com>
2024-01-22 09:00:18 -0500

    Setup default profile for firefox.

--- os/apps/web.nix
+++ os/apps/web.nix
@@ -1,4 +1,24 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, ... }: let 
+  inherit (inputs) nixpkgs home-manager;
+  inherit (config) allowUnfree;
+in 
 {
+  home-manager.users.gen = {
+    home.packages = with pkgs; [
+      firefox
+    ];
 
+    programs.firefox = {
+      enable = true;
+      profiles = {
+        default = {
+          id = 0;
+          name = "default";
+          settings = {
+            "browser.startup.homepage" = "about:blank";
+          };
+        };
+      };
+    };
+  };
 }
