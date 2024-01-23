5de98aaad69ecadbc21fc7dd71136034c2bfc1ab
Lucas Priestley <lmpriestley@outlook.com>
2024-01-23 09:05:49 -0500

    Scaffold hosts file for networking future hosts together.

--- os/components/networked.nix
+++ os/components/networked.nix
@@ -1,7 +1,15 @@
-{ networking, ... }:
-{
+{ networking, ... }: let
+  # TODO make IP static through nix managed router
+  hosts = {
+    gdw = "192.168.1.124";
+  };
+in {
   networking = {
     networkmanager.enable = true;
     firewall.enable = true;
+    hosts = {
+      "gdw" = [ hosts.gdw ];
+      "zabbix" = [ hosts.gdw ];
+    };
   };
 }
