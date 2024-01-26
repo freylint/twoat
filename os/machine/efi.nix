3aeea30d7e34abaab11c45c10fbc4fd1af676751
Lucas Priestley <lmpriestley@outlook.com>
2024-01-26 05:36:11 -0500

    Create basic disko config for an EFI machine.

---
+++ os/machine/efi.nix
@@ -0,0 +1,34 @@
+{ inputs, ... }: let 
+  inherit (inupts) disko;
+in {
+  disko.devices = {
+    disk.disk1 = {
+      device = lib.mkDefault "/dev/vda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          esp = {
+            name = "esp";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    }
+  };
+}
